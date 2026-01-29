aa6e7145e3dbf5115b8dd28b472382cfca3ad498
Rahel <rahel.aberare@gmail.com>
2026-01-30 00:36:00 +0300

    Primitive types and expressions

--- Program.cs
+++ Program.cs
@@ -1,12 +1,87 @@
-﻿using System;
-namespace HelloWorld
+﻿namespace HelloWorld
 {
     class Program
     {
         static void Main(string[] args)
         {
+            SayHello();
+            Variables();
+            TypeConversion();
+
+        }  
+        static void SayHello()
+        {
             Console.WriteLine("Hello, World!");
             Console.Beep();
-        }    
-    }
+        }   
+        static void Variables()
+        {
+            var number = 2;
+            var count = 10;
+            var totalPrice = 20.95f;
+            var character = 'A'; 
+            var firstName = "Jhon";
+            var isWorking = false;
+            Console.WriteLine(number);
+            Console.WriteLine(count);
+            Console.WriteLine(firstName);
+            Console.WriteLine(isWorking);
+            Console.WriteLine(totalPrice);
+            Console.WriteLine(character);
+            Console.WriteLine("{0} {1}", byte.MinValue, byte.MaxValue);
+            Console.WriteLine("{0} {1}", float.MinValue, float.MaxValue);
+            const float Pi = 3.14f;
+            Console.WriteLine(Pi);
+        }
+        static void TypeConversion()
+        {
+            byte b = 1;
+            int i = b; // implicit conversion
+            Console.WriteLine(i);
+
+            int i2 = 1;
+            byte b2 = (byte)i2; // explicit conversion
+            Console.WriteLine(b2);
+
+            string str = "1234";
+            int i3 = Convert.ToInt32(str); // conversion using Convert class
+            Console.WriteLine(i3);
+
+            try
+            {
+                string str2 = "1234abc";
+                int i4 = int.Parse(str2); // conversion using Parse method
+                Console.WriteLine(i4);
+            }
+            catch (FormatException)
+            {
+                Console.WriteLine("The string is not in a correct format.");
+            } 
+        }
+
+        static void Operators()
+        {
+            int a = 10;
+            int b = 3;
+            int c = 4;
+
+            Console.WriteLine(a + b); // Addition
+            Console.WriteLine(a - b); // Subtraction
+            Console.WriteLine(a * b); // Multiplication
+            Console.WriteLine(a / b); // Division
+            Console.WriteLine((float)a / (float)b); // Division with float result
+            Console.WriteLine(a % b); // Modulus
+
+            a++; // Increment
+            Console.WriteLine(a);
+            b--; // Decrement
+            Console.WriteLine(b); 
+            Console.WriteLine(a+ b * c); // Operator precedence
+            Console.WriteLine(a > b && a > c); // Logical AND
+            Console.WriteLine(a > b || a < c); // Logical OR
+            Console.WriteLine(!(a > b)); // Logical NOT
+            Console.WriteLine(a == b); // Equality
+            Console.WriteLine(a != b); // Inequality
+        }
+    }     
 }
--- obj/Debug/net10.0/C-sharp.AssemblyInfo.cs
+++ obj/Debug/net10.0/C-sharp.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("C-sharp")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+2bb9e2c512488292c9255bba1636067c802e1548")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+60eb7054a3317a46f8bcdeb32ed0f4237aaf1e38")]
 [assembly: System.Reflection.AssemblyProductAttribute("C-sharp")]
 [assembly: System.Reflection.AssemblyTitleAttribute("C-sharp")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
