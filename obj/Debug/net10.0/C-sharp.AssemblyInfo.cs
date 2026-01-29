60eb7054a3317a46f8bcdeb32ed0f4237aaf1e38
Rahel <rahel.aberare@gmail.com>
2026-01-29 12:55:41 +0300

    Add initial project structure with HelloWorld program and solution file

--- obj/Debug/net10.0/C-sharp.AssemblyInfo.cs
+++ obj/Debug/net10.0/C-sharp.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("C-sharp")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+a3844b91a37008d78cbfd80b550e37879f22a84a")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+2bb9e2c512488292c9255bba1636067c802e1548")]
 [assembly: System.Reflection.AssemblyProductAttribute("C-sharp")]
 [assembly: System.Reflection.AssemblyTitleAttribute("C-sharp")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
